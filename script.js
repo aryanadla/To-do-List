df40cc558ebe9ceacc01e9d9b146e4105c6ac133
Parth Seth <parthseth2004@gmail.com>
2023-04-16 12:23:12 +0530

    spacing

--- script.js
+++ script.js
@@ -2,13 +2,8 @@
 function handleSubmit(event,id){
 event.preventDefault();
     const txt=document.getElementById(id).value;
-    //alert("jo");
-    // make a item in unordered list and add it
-    //alert(txt);
-
     const newItem=document.createElement('li');
     newItem.textContent=txt;
     document.getElementById('list').appendChild(newItem);
-
     document.getElementById(id).value='';
 }
