b65dea57097567aab6be4e06d03fba480fb45810
Parth Seth <parthseth2004@gmail.com>
2023-06-14 00:46:43 +0530

    change hyperlinks

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ function handleSubmit(event, id) {
   const link = document.createElement('a');
   link.innerHTML = `<strong>${txt}</strong>`;
   if (!txt.startsWith('https://') && !txt.startsWith('http://')) {
-  txt = 'https://' + txt; // Prepend "https://" to the URL
-}
+    txt = 'https://' + txt; // Prepend "https://" to the URL
+  }
   link.href = txt; // Set the entered text as the URL for the link
   link.target = '_blank'; // Open the link in a new tab/window
-  
-  link.addEventListener('click', function () {
+
+  const doneButton = document.createElement('button');
+  doneButton.innerHTML = 'Done';
+
+  doneButton.addEventListener('click', function () {
     newItem.remove();
   });
 
   newItem.appendChild(link);
+  newItem.appendChild(doneButton);
+
   document.getElementById('list').appendChild(newItem);
   document.getElementById(id).value = '';
 }
