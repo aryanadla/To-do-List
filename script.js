45b2c179a7d6ed4521c2729ea5e46aa98ba3f97c
Uday <udayomsrivastava111@gmail.com>
2023-10-28 12:19:07 +0530

    only add to todo when txt is not empty, and some style changes to buttons and footer

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 function handleSubmit(event, id) {
-  event.preventDefault();
-  var txt = document.getElementById(id).value;
-  const newItem = document.createElement('li');
-  const link = document.createElement('a');
-  link.innerHTML = `<strong>${txt}</strong>`;
-  if (!txt.startsWith('https://') && !txt.startsWith('http://')) {
-    txt = 'https://' + txt; // Prepend "https://" to the URL
-  }
-  link.href = txt; // Set the entered text as the URL for the link
-  link.target = '_blank'; // Open the link in a new tab/window
+    event.preventDefault();
 
-  const doneButton = document.createElement('button');
-  doneButton.innerHTML = 'Done';
+    var txt = document.getElementById(id).value;
 
-  doneButton.addEventListener('click', function () {
-    newItem.remove();
-  });
+    if (txt == "") return;
 
-  newItem.appendChild(link);
-  newItem.appendChild(doneButton);
+    const newItem = document.createElement("li");
+    const link = document.createElement("a");
 
-  document.getElementById('list').appendChild(newItem);
-  document.getElementById(id).value = '';
+    link.innerHTML = `<strong>${txt}</strong>`;
+
+    if (!txt.startsWith("https://") && !txt.startsWith("http://")) {
+        txt = "https://" + txt; // Prepend "https://" to the URL
+    }
+    link.href = txt; // Set the entered text as the URL for the link
+    link.target = "_blank"; // Open the link in a new tab/window
+
+    const doneButton = document.createElement("button");
+    doneButton.innerHTML = "Done";
+
+    doneButton.addEventListener("click", function () {
+        newItem.remove();
+    });
+
+    newItem.appendChild(link);
+    newItem.appendChild(doneButton);
+
+    document.getElementById("list").appendChild(newItem);
+    document.getElementById(id).value = "";
 }
